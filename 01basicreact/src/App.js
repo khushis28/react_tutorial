360a8239572b60a699248191161c39acd60177d6
Khushi Singh <khushi@example.com>
2025-06-11 17:58:40 +0530

    new commit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+     <h1>Welcome to React | KS</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+
+
+  return (
+    <h1>Welcome to React with Vite | KS</h1>
+  )
+}
+
+export default App
