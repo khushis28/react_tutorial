a10363304228b827210b1e16a910c5f5c7fd2891
Khushi Singh <khushi@example.com>
2025-06-14 00:44:15 +0530

    neww

---
+++ counter-project/src/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./About.css";
+
+const About =()=>{
+    return(
+        <div className="constainer2">
+            This is about Page
+        </div>
+    )
+}
+
+export default About;
---
+++ counter-project/src/App.jsx
@@ -0,0 +1,16 @@
+import About from './About'
+import './App.css'
+
+function App() {
+  return (
+   <>
+    <div className='container'>
+       hii khushi how are you today
+    </div>
+    <About/>
+   </>
+
+  )
+}
+
+export default App
